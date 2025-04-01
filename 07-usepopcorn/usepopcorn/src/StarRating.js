66336d7000251a24dd4c46a19d950de59157eabe
gabegalindo <gorg2003@gmail.com>
2025-03-31 19:03:08 -0700

    update completed usepopcorn

--- 07-usepopcorn/usepopcorn/src/StarRating.js
+++ 07-usepopcorn/usepopcorn/src/StarRating.js
@@ -1,4 +1,5 @@
-import { use, useState } from "react";
+import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -10,6 +11,16 @@ const starContainerStyle = {
   display: "flex",
 };
 
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  className: PropTypes.string,
+  messages: PropTypes.array,
+  onSetRating: PropTypes.func,
+};
+
 export default function StarRating({
   maxRating = 5,
   color = "#fcc419",
@@ -17,12 +28,14 @@ export default function StarRating({
   className = "",
   messages = [],
   defaultRating = 0,
+  onSetRating,
 }) {
   const [rating, setRating] = useState(defaultRating);
   const [tmpRating, setTmpRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
+    onSetRating(rating);
   }
 
   const textStyle = {
