d9c0818d02fee2b1d84ab64ef7c13528311d6145
gabegalindo <gorg2003@gmail.com>
2025-06-10 10:01:01 -0700

    update: implemented routes and links to create a single page application

---
+++ 11-worldwise/worldwise/src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import PageNav from "../Components/PageNav";
+
+function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>Worldwise</h1>
+
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ 11-worldwise/worldwise/src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../Components/PageNav";
+
+function PageNotFound() {
+  return (
+    <div>
+      <PageNav />
+      Not found :(
+    </div>
+  );
+}
+
+export default PageNotFound;
