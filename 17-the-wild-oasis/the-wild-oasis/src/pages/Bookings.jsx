67e47eb153a59cd3a17fad23e8f95b10656d6ed4
gabegalindo <gorg2003@gmail.com>
2026-01-14 16:38:32 -0800

    update: implemented reusable pagination component

--- 17-the-wild-oasis/the-wild-oasis/src/pages/Bookings.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/pages/Bookings.jsx
@@ -1,13 +1,9 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import BookingTable from "../features/bookings/BookingTable";
-import useBookings from "../features/bookings/useBookings";
 import BookingTableOperations from "../features/bookings/BookingTableOperations";
 
 function Bookings() {
-  const res = useBookings();
-  console.log(res);
-
   return (
     <>
       <Row type="horizontal">
