de2a1dae34c7251119c1849e9f885036fa4a8d62
gabegalindo <gorg2003@gmail.com>
2026-01-22 17:15:02 -0800

    update: implemented authorization

--- 17-the-wild-oasis/the-wild-oasis/src/ui/ProtectedRoute.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/ui/ProtectedRoute.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { useUser } from "../features/authentication/useUser";
 import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const FullPage = styled.div`
   height: 100vh;
@@ -11,10 +13,20 @@ const FullPage = styled.div`
 `;
 
 function ProtectedRoute({ children }) {
-  // 1. Load the authenticated user
-  const { user, isLoading } = useUser();
+  const navigate = useNavigate();
 
-  // 2. While loading, show a spinner
+  // 1. Load the authenticated user
+  const { isLoading, isAuthenticated } = useUser();
+
+  // 2. If there is NO authenticated user, redirect to the /login
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate("/login");
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+
+  // 3. While loading, show a spinner
   if (isLoading)
     return (
       <FullPage>
@@ -22,11 +34,8 @@ function ProtectedRoute({ children }) {
       </FullPage>
     );
 
-  // 3. If there is NO authenticated user, redirect to the /login
-
   // 4. If there IS a user, render the app
-
-  return children;
+  if (isAuthenticated) return children;
 }
 
 export default ProtectedRoute;
